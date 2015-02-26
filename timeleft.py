299a99a2d5116963a1bba22b230814bf39e6cbae
Thomas Schraitle <tom_schr@web.de>
2015-02-26 13:53:19 +0100

    Added OAuth in user GitHub URL

--- timeleft.py
+++ timeleft.py
@@ -5,41 +5,58 @@ import time
 import requests
 
 # URL='https://api.github.com/repos/{user}/{repo}'
-URL='https://api.github.com/users/{user}'
+URL='https://api.github.com/users/{user}?client_id={clientid}&client_secret={clientsecret}'
 
 class SomethingWrong(requests.RequestException):
     pass
 
-def getxratelimitreset(user):
+def fixurl(user):
+    from config import TOKEN, CLIENTID, CLIENTSECRET
+    return URL.format(user=user, clientid=CLIENTID, clientsecret=CLIENTSECRET)
+
+
+def getxratelimits(user):
     """
     """
-    from config import TOKEN
-    header = { 'Authorization': "token {}".format(TOKEN) }
+    
     session = requests.Session()
-    r = session.get(URL.format(user=user), headers=header)
+    r = session.get( fixurl(user) )
 
     if not r.ok or r.status_code != requests.codes.ok:
         raise SomethingWrong(r.reason, r)
 
-    epoch = r.headers.get("X-RateLimit-Reset", None)
-    if not epoch:
-        raise AttributeError("Could not find 'X-RateLimit-Reset' in HTTP header.", r)
+    X = {
+        "X-RateLimit-Reset":     None,
+        "X-RateLimit-Remaining": None,
+        "X-RateLimit-Limit":     None,
+        }
 
-    r.close()
+    try:
+        for key in X.keys():
+            X[key] = r.headers.get(key, None)
+            X[key] = int(X[key])
+    except TypeError:
+        raise AttributeError("Could not find '{}' in HTTP header.".format(key), r)
+    finally:
+        r.close()
 
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(epoch)))
+    reset="X-RateLimit-Reset"
+    X[reset] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(X[reset]))
+    return X
 
 
 if __name__ == "__main__":
     user="tomschr"
 
     try:
-        epoch = getxratelimitreset(user)
-        print("Next time for '{user}' is {epoch}".format(**locals()))
+        result = getxratelimits(user)
+        for key in result:
+            print("{} = {}".format(key, result[key]))
+
     except (SomethingWrong, AttributeError) as err:
         print(err.args[0], file=sys.stderr)
         err.args[1].close()
     except ImportError as err:
-        print("Missing config.py not found", file=sys.stderr)
-        
+        print("Missing config.py not found\n  {}".format(err), file=sys.stderr)
+
 # EOF
