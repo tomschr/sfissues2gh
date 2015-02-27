736c38992a840d95f5c26d432977066da92906d3
Thomas Schraitle <tom_schr@web.de>
2015-02-27 13:15:49 +0100

    Setup logging

--- issue2gh.py
+++ issue2gh.py
@@ -22,14 +22,8 @@ __author__ = "Thomas Schraitle <toms@opensuse.org>"
 
 log = logging.getLogger(__file__)
 formatter = logging.Formatter('[%(levelname)s] %(message)s')
-
 handler = logging.StreamHandler(stream=sys.stdout)
 handler.setFormatter(formatter)
-handler.setLevel(logging.DEBUG)
-
-log.addHandler(handler)
-log.setLevel(logging.DEBUG)
-
 
 
 def migrateTickets():
@@ -41,7 +35,7 @@ def getCollaborators(git, repo):
 def getMilestones(git, repo):
     return repo.get_milestones()
 
-    
+
 def setupGitHubRepo(login="tomschr", reponame="daps-test"):
     git = github.Github(TOKEN)
     repo = git.get_repo('{0}/{1}'.format(login, reponame))
@@ -53,7 +47,7 @@ def getIssues(git, repo):
 
 def getGHuser(sfuser):
     return SF2GHuserdict.get(sfuser)
-    
+
 def getSFuser(ghuser):
     return GH2SFuserdict.get(ghuser)
 
@@ -67,16 +61,16 @@ def getGHUsers(git):
         except github.UnknownObjectException:
             log.warn("Couldn't find a matching GitHub user '{}' for {}".format(u, getSFuser(u) ) )
     return res
-    
+
 def parser():
     import argparse
     usage = """
     """
     parser = argparse.ArgumentParser(prog=__file__)
     parser.add_argument('jsonfile', help="JSON export from Sourceforge")
-    parser.add_argument('repo', 
+    parser.add_argument('repo',
         nargs="?",
-        help="Repo name as <owner>/<project>", 
+        help="Repo name as <owner>/<project>",
         default=DEFAULTREPO,
         )
     parser.add_argument('-d', '--debug',
@@ -84,28 +78,54 @@ def parser():
         action="store_true",
         default=False,
         help="Help debugging")
-
-    parser.add_argument('-s', '--start', 
-        dest='start_id', 
+    parser.add_argument('-v', '--verbose',
+        dest="verbose",
+        action="count",
+        help="Raise verbosity level",
+        )
+    parser.add_argument('-s', '--start',
+        dest='start_id',
         action='store',
         help='id of first issue to import (inclusive); useful for aborted runs')
-    parser.add_argument('-e', '--end', 
-        dest='end_id', 
+    parser.add_argument('-e', '--end',
+        dest='end_id',
         action='store',
         help='id of end issue to import (inclusive); useful for aborted runs')
     parser.add_argument('-u', '--user',
         dest='github_user')
-    parser.add_argument("-T", "--no-id-in-title", 
+    parser.add_argument("-T", "--no-id-in-title",
         action="store_true",
-        dest="no_id_in_title", 
+        dest="no_id_in_title",
         help="do not append '[sf#12345]' to issue titles")
     #parser.add_argument('-U', '--user-map',
     #    help="A json file mapping SF username to GitHub username",
     #    default={},
     #    type=load_json)
-    
+
     args = parser.parse_args()
     return args
+
+def setLogging(args, examples=True):
+   leveldict = {
+      None: logging.ERROR,  # logging.NOTSET,
+         1: logging.WARNING,
+         2: logging.INFO,
+         3: logging.DEBUG
+       }
+
+   handler.setLevel(leveldict.get(args.verbose, logging.DEBUG))
+
+   log.addHandler(handler)
+   log.setLevel(leveldict.get(args.verbose, logging.DEBUG))
+
+   if examples:
+      log.debug("Debug")
+      log.info("Info")
+      log.warn("Warning")
+      log.error("Error")
+      log.critical("OHje!!")
+
+
 
 if __name__ == "__main__":
    args = parser()
@@ -116,6 +136,10 @@ if __name__ == "__main__":
       log.error("The JSON file '{}' does not exists.".format(args.jsonfile))
       sys.exit(10)
 
+   print(">>>", args)
+   setLogging(args)
+
+
    r = args.repo.split("/") if args.repo else DEFAULTREPO.split("/")
 
    log.info("Using repo {}".format(r))
@@ -123,9 +147,8 @@ if __name__ == "__main__":
    g = github3.login(token=TOKEN)
    repo = g.repository(*r)
 
-   print("URL    : {}".format(repo.url))
-   print("Git URL: {}".format(repo.git_url))
-
+   log.info("URL    : {}".format(repo.url))
+   log.info("Git URL: {}".format(repo.git_url))
 
    missingcollabs=[]
    found=[]
